ebff96130e0cac028e995a181f9feaabf30859f4
Artem Drushchyts <temafm-3@yandex.ru>
2020-02-10 19:06:33 +0300

    refactor: break the project into components

--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
 import React from "react";
 import ReactDOM from 'react-dom';
+import SearchPanel from "./components/search-panel";
+import AppHeader from "./components/app-header";
+import TodoList from "./components/todo-list";
 
-const AppHeader = () => {
-    return <h1>My Todo List</h1>
-}
-
-const SearchPanel = () => {
-    return <input placeholder='search'/>
-}
-
-const TodoList = () => {
-    return (
-        <ul>
-            <li>Learn React</li>
-            <li>Build Awesome App</li>
-        </ul>
-    )
-}
 const App = () => {
     return (
         <div>
