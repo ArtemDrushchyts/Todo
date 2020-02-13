8aec269c87581ad7c9ebf94665a23dbf307556e8
Artem Drushchyts <temafm-3@yandex.ru>
2020-02-13 20:46:51 +0300

    refactor: add structure folder

--- src/index.js
+++ src/index.js
@@ -1,25 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import SearchPanel from "./components/search-panel";
-import AppHeader from "./components/app-header";
-import TodoList from "./components/todo-list";
-import './index.css';
 
-const App = () => {
-
-    const todoData = [
-        {label: 'Drink Coffee', important: false, id: 1},
-        {label: 'Make Awesome App', important: true, id: 2},
-        {label: 'Have a lunch', important: false, id: 3},
-    ];
-
-    return (
-        <div className='todo-app'>
-            <AppHeader/>
-            <SearchPanel/>
-            <TodoList todos={todoData}/>
-        </div>
-    )
-}
+import App from './components/app';
 
  ReactDOM.render(<App/>, document.getElementById('root'))
